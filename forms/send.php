8aa5cecaf62472f27ab0778f6143b9294a73d5d2
Oriva <weelman93@gmail.com>
2018-11-13 15:50:03 +0800

    save

--- forms/send.php
+++ forms/send.php
@@ -1,21 +1,32 @@
 <?
-    $to = 'weelman93@gmail.com';
-    $subject = 'Заявка с сайта тенниса!';
-    $message = '
+$to = 'weelman93@gmail.com';
+$subject = 'Заявка с сайта тенниса!';
+$name=''; $phone=''; $question=''; $occupation='';
+if (!empty($_POST["name"])) {
+    $name = '<p style = "font-size: 1.2em; color: #333" ><span style = "font-weight: 600" > Имя:</span > '.$_POST['name'].' </p>';
+};
+if(!empty($_POST["phone"])) {
+    $phone = '<p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Телефон:</span> ' . $_POST['phone'] . '</p>';
+};
+if(!empty($_POST["question"])) {
+    $question = '<p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Вопрос:</span> ' . $_POST['question'] . '</p>';
+}
+if(!empty($_POST["occupation"])) {
+    $occupation = '<p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Тип занятия:</span> ' . $_POST['occupation'] . '</p>';
+}
+$message = '
                 <html>
                     <head>
-                        <title>'.$subject.'</title>
+                        <title>' . $subject . '</title>
                     </head>
                     <body style="padding: 20px; border: 1px solid #000">
-                        <img src="https://tennis-irkutsk.ru/img/logo.png" style="display: block; width: 50px; margin: 20px;">
-                        <p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Тип заявки:</span> '.$_POST['about-form'].'</p>
-                        <p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Имя:</span> '.$_POST['name'].'</p>
-                        <p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Телефон:</span> '.$_POST['phone'].'</p>
-                        <p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Вопрос:</span> '.$_POST['question'].'</p>
-                        <p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Тип занятия:</span> '.$_POST['occupation'].'</p>
-                    </body>
+                        <img src="http://tennis-irkutsk.ru/img/logo.png" style="display: block; width: 50px; margin: 20px;">
+                        <p style="font-size: 1.2em; color: #333"><span style="font-weight: 600">Тип заявки:</span> ' . $_POST['about-form'] . '</p>'
+                        . $name . $phone . $question . $occupation .
+                    '</body>
                 </html>';
-    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-    $headers .= "From: http://tennis-irkutsk.ru/ <site@tennis-irkutsk.ru>\r\n";
-    mail($to, $subject, $message, $headers);
+echo $message;
+$headers = "Content-type: text/html; charset=utf-8 \r\n";
+$headers .= "From: https://tennis-irkutsk.ru/ <site@tennis-irkutsk.ru>\r\n";
+mail($to, $subject, $message, $headers);
 ?>
